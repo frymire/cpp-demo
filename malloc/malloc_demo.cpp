0957f7fe258dcc88ec030bfce7999d3832c56c1b
Mark Frymire <Mark.E.Frymire@wbbinc.com>
2020-11-25 16:55:09 -0500

    Refactor malloc_demo

--- malloc/malloc_demo.cpp
+++ malloc/malloc_demo.cpp
@@ -1,29 +1,30 @@
+
+// Need this to use strcpy and strcat, which are potentially unsafe.
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void printIt(char *myString) {
-	printf("String = %s, hex address = %p, unsigned int address = %u\n", myString, myString, (unsigned int) myString);
+void printIt(char* str) {
+  printf("String = %s, hex address = %p, unsigned int address = %u\n", str, str, (unsigned int) str);
 }
 
-int main() {
+void main() {
 
-	// Allocate memory for a string. In C++, you have to explicitly cast the return type.
-	printf("Allocating memory...\n"); 
-	char* myString = (char*) malloc(15);
-	strcpy(myString, "tutorialspoint");
-	printIt(myString);
+  // Allocate memory for a string. In C++, you have to explicitly cast the return type.
+  printf("Allocating memory...\n");
+  char* myString = (char*) malloc(12); // Length of the string itself, plus 1 for the null terminator.
+  strcpy(myString, "MarkFrymire");
+  printIt(myString);
 
-	// Reallocate the memory.
-	printf("\nReallocating memory...\n");
-	myString = (char*) realloc(myString, 25);
-	printIt(myString);
+  // Reallocate the memory.
+  printf("\nReallocating memory...\n");
+  myString = (char*) realloc(myString, 25);
+  printIt(myString);
 
-	strcat(myString, ".com");
-	printIt(myString);
+  strcat(myString, ".com");
+  printIt(myString);
 
-	free(myString);
-	return 0;
+  free(myString);
 }
