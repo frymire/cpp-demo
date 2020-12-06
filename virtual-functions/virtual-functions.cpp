3c7d214fe1b9bd4c59133d154106e512efb64dfc
Mark Frymire <Mark.E.Frymire@wbbinc.com>
2020-12-06 16:30:27 -0500

    Demo virtual destructors

--- virtual-functions/virtual-functions.cpp
+++ virtual-functions/virtual-functions.cpp
@@ -1,6 +1,8 @@
 
 // Demo virtual functions and interfaces
-// See here: https://www.youtube.com/watch?v=UWAdd13EfM8&list=WL&index=67&t=18s
+// See here: https://www.youtube.com/watch?v=oIV2KchSyGQ
+// And here: https://www.youtube.com/watch?v=UWAdd13EfM8&list=WL&index=67&t=18s
+// And here: https://www.youtube.com/watch?v=jELbKhGkEi0
 
 #include <iostream>
 using std::cout;
@@ -15,11 +17,21 @@ public:
   virtual string GetClassName() = 0;
 };
 
-void Print(Printable* obj) { cout << obj->GetClassName() << endl; }
+void PrintClassName(Printable* obj) { cout << "Class name is " << obj->GetClassName() << endl; }
 
 class Entity: public Printable {
 public:
-  virtual void SayHi() { cout << "Hi.\n"; } // virtual
+  Entity() { cout << "Constructed an Entity.\n"; }
+
+  // Key point here. Suppose you instantiate an Entity polymorphically by assigning an Entity* 
+  // to an instance of a derived class (e.g. Player below). Without adding the "virtual" keyword
+  // to the base class destructor, the destructor of the derived class would not be called,
+  // probably resulting in a memory leak. Unlike virtual functions on normal functions, with
+  // destructors, C++ calls *both* the base class and the derived class destructor, rather than
+  // just overriding the base class function.
+  virtual ~Entity() { cout << "Destroyed an Entity.\n"; } // try without "virtual"
+  
+  virtual void SayHi() { cout << "Entity says Hi.\n"; } // virtual
   void SayBye() { cout << "Entity says Bye.\n"; } // not virtual
   virtual string GetClassName() override { return "Entity"; }
   virtual string GetName() = 0; // pure virtual
@@ -32,12 +44,13 @@ private:
 
 public:
 
-  Player(const string& name): m_Name(name) {}
+  Player(const string& name): m_Name(name) { cout << "Constructed a Player.\n"; }
+  ~Player() { cout << "Destroyed a Player.\n"; }
 
   // The override keyword verifies that you're overriding a base class function.
   //  You can get away without it, but adding it is good practice.
   //void SayHii() override { cout << "Hey...\n"; } // compile error, doesn't override anything in Entity
-  void SayHi() override { cout << "Hey...\n"; } 
+  void SayHi() override { cout << "Player says Hey...\n"; } 
 
   // The base class function SayBye() was not declared virtual, so you can't override it here.
   // Declaring it here with the same signature might lead to surprising behavior if you instantiate Player
@@ -49,7 +62,6 @@ public:
 
   // By providing this definition of the pure virtual function in Entity, you can now instantiate Players.
   string GetName() override { return m_Name; }
-
 };
 
 int main() {
@@ -57,12 +69,18 @@ int main() {
   // You can't instantiate Entity directly, because GetName() is pure virtual.
   //Entity entity1(); // compile error
 
+  // Instantiate a Player polymorphically as an Entity.
   Entity* e = new Player("Mark");
   e->SayHi();
   e->SayBye(); // uses Entity::SayBye(), not Player::SayBye()!
-  Print(e);
+  PrintClassName(e);
   cout << e->GetName() << endl;
+  cout << "Deleting e...\n";
+  delete e;
 
+  cout << endl;
   Player* p = new Player("Kurt");
   p->SayBye(); // uses Player::SayBye()
+  cout << "Deleting p...\n";
+  delete p;
 }
