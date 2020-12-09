1a0b688ae41fb72070614c2cf7b1c857eb199e42
Mark Frymire <Mark.E.Frymire@wbbinc.com>
2020-12-08 22:42:24 -0500

    Remove high-resolution_clock from timing demo

--- timing/timing.cpp
+++ timing/timing.cpp
@@ -1,15 +1,16 @@
 
 // Demo a platform-independent method for getting timing.
 // See here: https://www.youtube.com/watch?v=oEx5vGNFrLk&list=WL&index=21
+// Note: I removed high_resolution_clock from this in favor of steady_clock, as 
+// recommended here: https://en.cppreference.com/w/cpp/chrono/high_resolution_clock
 
 #include <iostream>
 using std::cout;
 using std::endl;
 
 #include <chrono>
-using std::chrono::high_resolution_clock;
-using std::chrono::time_point;
 using std::chrono::steady_clock;
+using std::chrono::time_point;
 using std::chrono::duration;
 using namespace std::literals::chrono_literals;
 
@@ -20,16 +21,16 @@ struct Timer {
 
   time_point<steady_clock> start; 
 
-  Timer(): start(high_resolution_clock::now()) {}
+  Timer(): start(steady_clock::now()) {}
 
   ~Timer() { 
-    duration<float> duration = high_resolution_clock::now() - start;
+    duration<float> duration = steady_clock::now() - start;
     cout << duration.count() << " s" << endl;
   }
 };
 
 void MyFunction() {
-  Timer timer;
+  Timer timer; // automatically report timing when this function goes out of scope.
   std::this_thread::sleep_for(2s);
 }
 
@@ -37,9 +38,9 @@ int main() {
 
   // Long way...
   cout << "\nLong way (1 sec)..." << endl;
-  auto start = high_resolution_clock::now();
+  time_point<steady_clock> start = steady_clock::now();
   std::this_thread::sleep_for(1s);
-  duration<float> duration = high_resolution_clock::now() - start;
+  duration<float> duration = steady_clock::now() - start;
   cout << duration.count() << " s" << endl;
 
   // Clean way using object lifetime...
