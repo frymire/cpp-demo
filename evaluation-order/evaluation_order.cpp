641a935b32e9cd2822af3b552dad361514749b6b
Mark Frymire <Mark.E.Frymire@wbbinc.com>
2020-12-17 17:05:04 -0500

    Demo move semantics between existing objects

--- evaluation-order/evaluation_order.cpp
+++ evaluation-order/evaluation_order.cpp
@@ -2,6 +2,8 @@
 // Demonstrate some subtle evaluation order issues.
 // See here: https://www.youtube.com/watch?v=qYxTP7wrCho&list=WL&index=18&t=263s
 
+// You can easily compile the following code using different compilers at wandbox.org.
+
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -18,10 +20,8 @@ int main() {
   // For the following line of code, the result is 1 + 0 = 1 in Debug, and 0 + 0 = 0 in Release.
   // The reason for getting two zeros in the second case is because the compiler is allowed to 
   // work out the values in parallel. This is changed in C++17, and Release also results in
-  // 1 + 0 = 1.
+  // 1 + 0 = 1, but the rules also allow 0 + 1 = 1 (which is given by C++14 and Clang).
   PrintSum(value++, value++);
-
-
 
   value = 0;
   PrintSum(++value, ++value); // 2 + 2 = 4 in Debug and Release
--- move/move.cpp
+++ move/move.cpp
@@ -7,16 +7,18 @@
 using std::cout;
 using std::endl;
 
-class MyString {
+using std::move;
+
+class MovableString {
 
   char* m_data;
   size_t m_size; // size_t is the type returned by strlen()
 
 public:
   
-  MyString() = default;
+  MovableString() = default;
   
-  MyString(const char* str) { 
+  MovableString(const char* str) { 
     m_size = strlen(str);
     m_data = new char[m_size];
     memcpy(m_data, str, m_size);
@@ -24,7 +26,7 @@ public:
   }
 
   // Copy constructor (can pass an lvalue or rvalue parameter).
-  MyString(const MyString& that) {
+  MovableString(const MovableString& that) {
     m_size = that.m_size;
     m_data = new char[m_size]; // allocate memory
     memcpy(m_data, that.m_data, m_size); // copy everything into it
@@ -32,7 +34,7 @@ public:
   }
 
   // Move constructor (requires rvalue parameter).
-  MyString(MyString&& that) noexcept {
+  MovableString(MovableString&& that) noexcept {
 
     m_size = that.m_size;
     m_data = that.m_data; // can just assign the pointer, because that is an rvalue
@@ -45,10 +47,30 @@ public:
     printf("Moved (size = %d)!\n", m_size);
   }
 
-  ~MyString() { 
+  ~MovableString() { 
     delete m_data; 
     printf("Destroyed (size = %d)!\n", m_size);
   }
+
+  MovableString& operator=(MovableString&& that) noexcept {
+
+    // If we get a request to move this object to itself, do nothing.
+    if (this == &that) return *this;
+
+    // First, delete data for the current MovableString.
+    delete[] m_data;
+    printf("Deleted data (size = %d)!\n", m_size);
+
+    // Then, do the same steps as in the move constructor...
+    m_size = that.m_size;
+    m_data = that.m_data;
+    that.m_size = 0;
+    that.m_data = nullptr;
+    printf("Moved (size = %d)!\n", m_size);
+
+    return *this;
+  }
+
 
   void Print() {
     for (int i = 0; i < m_size; i++) printf("%c", m_data[i]);
@@ -59,21 +81,59 @@ public:
 
 class Entity {
 
-  MyString m_name;
+  MovableString m_name;
 
 public:
 
-  Entity(const MyString& name): m_name(name) {}
+  Entity(const MovableString& name): m_name(name) {}
 
-  // Provide an Entity constructor that ensures the use of the MyString copy constructor. 
-  //Entity(MyString&& name): m_name(name) {} // no, still uses MyString copy constructor to set m_name
-  //Entity(MyString&& name): m_name((MyString&&) name) {} // casting to (MyString&&) works, but rarely used
-  Entity(MyString&& name): m_name(std::move(name)) {} // best
+  // Construct an Entity while ensuring the use of the MovableString move constructor. 
+  //Entity(MovableString&& name): m_name(name) {} // no, still uses MovableString copy constructor to set m_name
+  //Entity(MovableString&& name): m_name((MovableString&&) name) {} // casting to (MovableString&&) works, but rarely used
+  Entity(MovableString&& name): m_name(move(name)) {} // yes, uses move constructor
   
   void PrintName() { m_name.Print(); }
 };
 
 int main() {
-  Entity e(MyString("Mark"));
-  e.PrintName();
+
+  cout << "\nUse the explicit MovableString(const char*) constructor to instantiate an entity...\n";
+  Entity e1(MovableString("Mark")); // creates the string on the stack and then moves it
+  e1.PrintName();
+
+  cout << "\nUse the implicit conversion to instantiate an entity...\n";
+  Entity e2("Frymire");
+  e2.PrintName();
+
+  cout << "\nCopy from a source to a destination string...\n";
+  MovableString source1 = "source";
+  source1.Print();
+  MovableString copied_string = source1;
+  copied_string.Print();
+
+  cout << "\nMove from a source to a new destination string by casting...\n";
+  MovableString source2 = "source";
+  source2.Print();
+  MovableString moved_with_cast = (MovableString&&) source2; // cast to an rvalue
+  // equivalent to "MovableString moved_with_cast((MovableString&&) source2);"
+  moved_with_cast.Print();
+
+  cout << "\nMove from a source to a new destination string using std::move...\n";
+  MovableString source3 = "source";
+  source3.Print();
+  MovableString moved_with_std_move(move(source3)); // converts source3 to an rvalue
+  // You could also say "MovableString moved_with_std_move = move(source);" 
+  // This looks like an assignment, but is actually a constructor.
+  moved_with_std_move.Print();
+
+  cout << "\nMove from a source to an existing destination string using std::move...\n";
+  MovableString source4 = "source";
+  MovableString existing_string = "existing";
+  source4.Print();
+  existing_string.Print();
+  existing_string = move(source4); // calls operator=, not a constructor
+  source4.Print(); // empty data, but still a valid MovableString
+  existing_string.Print();
+
+  cout << "\nCall destructors...\n";
 }
