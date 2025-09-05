220b4d719e159da336d65eacb7e87c8d7860bc39
Mark Frymire <mark.frymire@icloud.com>
2025-09-05 12:06:00 -0400

    Tidy up std::move demo

--- modules/move/move.cpp
+++ modules/move/move.cpp
@@ -1,5 +1,4 @@
-
-// Demostrate move semantics (new in C++11).
+// Demonstrate move semantics (new in C++11).
 // See here: https://www.youtube.com/watch?v=ehMg6zvXuMY&t=31s
 // And here: https://www.youtube.com/watch?v=OWNeCTd7yQE&list=WL&index=19&t=1s
 
@@ -7,8 +6,6 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-
-using std::move;
 
 class MovableString {
 
@@ -19,7 +16,7 @@ public:
   
   MovableString() = default;
   
-  MovableString(const char* str) { 
+  MovableString(const char* str) { // NOLINT(*-explicit-constructor)
     m_size = strlen(str);
     m_data = new char[m_size];
     memcpy(m_data, str, m_size);
@@ -73,7 +70,7 @@ public:
   }
 
 
-  void Print() {
+  void print() const {
     for (int i = 0; i < m_size; i++) printf("%c", m_data[i]);
     printf("\n");
   }
@@ -86,55 +83,55 @@ class Entity {
 
 public:
 
-  Entity(const MovableString& name): m_name(name) {}
+  Entity(const MovableString& name): m_name(name) {}  // NOLINT(*-explicit-constructor)
 
   // Construct an Entity while ensuring the use of the MovableString move constructor. 
   //Entity(MovableString&& name): m_name(name) {} // no, still uses MovableString copy constructor to set m_name
-  //Entity(MovableString&& name): m_name((MovableString&&) name) {} // casting to (MovableString&&) works, but rarely used
-  Entity(MovableString&& name): m_name(move(name)) {} // yes, uses move constructor
+  //Entity(MovableString&& name): m_name((MovableString&&) name) {} // casting works, but is poor style
+  Entity(MovableString&& name): m_name(std::move(name)) {} // yes, uses move constructor, NOLINT(*-explicit-constructor)
   
-  void PrintName() { m_name.Print(); }
+  void print_name() const { m_name.print(); }
 };
 
 int main() {
 
   cout << "\nUse the explicit MovableString(const char*) constructor to instantiate an entity...\n";
   Entity e1(MovableString("Mark")); // creates the string on the stack and then moves it
-  e1.PrintName();
+  e1.print_name();
 
   cout << "\nUse the implicit conversion to instantiate an entity...\n";
   Entity e2("Frymire");
-  e2.PrintName();
+  e2.print_name();
 
   cout << "\nCopy from a source to a destination string...\n";
   MovableString source1 = "source";
-  source1.Print();
-  MovableString copied_string = source1;
-  copied_string.Print();
+  source1.print();
+  MovableString copied_string = source1; // NOLINT(*-unnecessary-copy-initialization)
+  copied_string.print();
 
   cout << "\nMove from a source to a new destination string by casting...\n";
   MovableString source2 = "source";
-  source2.Print();
-  MovableString moved_with_cast = (MovableString&&) source2; // cast to an rvalue
+  source2.print();
+  MovableString moved_with_cast = static_cast<MovableString&&>(source2); // cast to an rvalue
   // equivalent to "MovableString moved_with_cast((MovableString&&) source2);"
-  moved_with_cast.Print();
+  moved_with_cast.print();
 
   cout << "\nMove from a source to a new destination string using std::move...\n";
   MovableString source3 = "source";
-  source3.Print();
-  MovableString moved_with_std_move(move(source3)); // converts source3 to an rvalue
-  // You could also say "MovableString moved_with_std_move = move(source);" 
+  source3.print();
+  MovableString moved_with_std_move(std::move(source3)); // converts source3 to an rvalue
+  // You could also say "MovableString moved_with_std_move = std::move(source);"
   // This looks like an assignment, but is actually a constructor.
-  moved_with_std_move.Print();
+  moved_with_std_move.print();
 
   cout << "\nMove from a source to an existing destination string using std::move...\n";
   MovableString source4 = "source";
   MovableString existing_string = "existing";
-  source4.Print();
-  existing_string.Print();
-  existing_string = move(source4); // calls operator=, not a constructor
-  source4.Print(); // empty data, but still a valid MovableString
-  existing_string.Print();
+  source4.print();
+  existing_string.print();
+  existing_string = std::move(source4); // calls operator=, not a constructor
+  source4.print(); // empty data, but still a valid MovableString
+  existing_string.print();
 
   cout << "\nCall destructors...\n";
 }
