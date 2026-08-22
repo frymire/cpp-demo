41b448931c71bf7acc5c4160d77fe1d6dea4dd4c
Mark Frymire <mark.frymire@icloud.com>
2026-08-22 18:10:54 -0400

    Update malloc demo

--- modules/02-pointers-and-memory/malloc/malloc_demo.cpp
+++ modules/02-pointers-and-memory/malloc/malloc_demo.cpp
@@ -2,30 +2,36 @@
 // Need this to use strcpy and strcat, which are potentially unsafe.
 // #define _CRT_SECURE_NO_WARNINGS
 
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cstdint>
+#include <cstdio>  // printf
+#include <cstdlib>  // malloc
+#include <cstring>  // strcpy
 
-void print_it(char* str) {
-  printf("String = %s, hex address = %p, unsigned int address = %lu\n", str, str, (uintptr_t) str);
+static void print_it(char* str) {
+  printf("String = %s, hex address = %p, unsigned int address = %llu\n", str, str, reinterpret_cast<uintptr_t>(str));
 }
 
 int main() {
 
   // Allocate memory for a string. In C++, you have to explicitly cast the return type.
   printf("Allocating memory...\n");
-  char* my_string = (char*) malloc(12); // Length of the string itself, plus 1 for the null terminator.
-  strcpy(my_string, "MarkFrymire");
-  print_it(my_string);
+  char* p_string = static_cast<char*>(malloc(12)); // length of the string itself, plus 1 for the null terminator
+  strcpy(p_string, "MarkFrymire");
+  print_it(p_string);
 
-  // Reallocate the memory.
-  printf("\nReallocating memory...\n");
-  my_string = (char*) realloc(my_string, 25);
-  print_it(my_string);
+  // Reallocate the memory. You might or might not get a new address.
+  printf("\nReallocating (and automatically copying) memory...\n");
+  char* p_reallocated_string = static_cast<char*>(realloc(p_string, 25));
+  if (p_reallocated_string == nullptr) {  // if realloc fails, it does not free the original block
+    free(p_string);
+    return 1;
+  }
+  p_string = p_reallocated_string;
+  print_it(p_string);
 
-  strcat(my_string, ".com");
-  print_it(my_string);
+  strcat(p_string, ".com");
+  print_it(p_string);
 
-  free(my_string);
+  free(p_string);
+
+  return 0;
 }
