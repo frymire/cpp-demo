59878214af00db783401ebb48f4034f641725113
Mark Frymire <mark.frymire@icloud.com>
2026-08-22 13:47:36 -0400

    Refactor binary operators demo

--- modules/01-basics/binary-operators/binary_operators.cpp
+++ modules/01-basics/binary-operators/binary_operators.cpp
@@ -10,30 +10,38 @@ using std::endl;
 #include <bitset> // represent values as bits
 using std::bitset;
 
-int main(void) {
+void print_bits(uint8_t byte) {
+  cout << bitset<8>(byte) << endl;
+}
 
-  unsigned int a = 0b00000101; // 5
-  unsigned int b = 0b00001100; // 12
-  cout << "a = " << bitset<8>(a) << endl;
-  cout << "b = " << bitset<8>(b) << endl;
+int main() {
+
+  constexpr uint8_t a = 0b00000101; // 5
+  constexpr uint8_t b = 0b00001100; // 12
+  cout << "a = " << static_cast<int>(a) << endl;
+  cout << "b = " << static_cast<int>(b) << endl;
+  cout << "a = "; print_bits(a);
+  cout << "b = "; print_bits(b);
 
   // Bit shift left (doubles a). Disassembly shows "shl (shift left) eax, 1".
-  cout << "(a << 1) = " << bitset<8>(a << 1) << endl; // 0000 1010
+  cout << "(a << 1) = "; print_bits(a << 1); // 0000 1010
 
   // Three bit shifts to the left (a*(2^3) = 40)
-  cout << "(a << 3) = " << bitset<8>(a << 3) << endl; // 0101 0000
+  cout << "(a << 3) = "; print_bits(a << 3); // 0101 0000
 
   // Bit shift left (halves a)
-  cout << "(a >> 1) = " << bitset<8>(a >> 1) << endl; // 0000 0010 (1) <- lost
+  cout << "(a >> 1) = "; print_bits(a >> 1); // 0000 0010 (1) <- lost
 
-  cout << "AND: (a & b) = " << bitset<8>(a & b) << endl; // 0000 0100
-  cout << "OR: (a | b) = " << bitset<8>(a | b) << endl; // 0000 1101
-  cout << "XOR: (a ^ b) = " << bitset<8>(a ^ b) << " (reports different bits)" << endl; // 0000 1001
-  cout << "NOT: (~a) = " << bitset<8>(~a) << endl; // 1111 1010
+  cout << "AND: (a & b) = "; print_bits(a & b); // 0000 0100
+  cout << "OR: (a | b) = "; print_bits(a | b); // 0000 1101
+  cout << "XOR: (a ^ b) = "; print_bits(a ^ b); // 0000 1001 (reports different bits)
+  cout << "NOT: (~a) = "; print_bits(~a); // 1111 1010
 
-  cout << "Get bit position 4 (zero-based): (bool) (0b01100101 & (1 << 4)) = " << (bool) (0b01100101 & (1 << 4)) << endl; // 0
-  cout << "Get bit position 5 (zero-based): (bool) (0b01100101 & (1 << 5)) = " << (bool) (0b01100101 & (1 << 5)) << endl; // 1
-  cout << "Set bit position 4: 0b10100101 | (1 << 3) = " << bitset<8>(0b10100101 | (1 << 3)) << endl;
-  cout << "Clear bit position 6: 0b10100101 & ~(1 << 5) = " << bitset<8>(0b10100101 & ~(1 << 5)) << endl;
-  cout << "Clear the middle 4 bits: 0b10100101 (target) & 0b11000011 (mask) = " << bitset<8>(0b10100101 & 0b11000011) << endl;  
+  cout << "Get bit 5: (bool) (0b01100101 & (1 << 4)) = " << static_cast<bool>(0b01100101 & (1 << 4)); // 0
+  cout << "\nGet bit 6: (bool) (0b01100101 & (1 << 5)) = " << static_cast<bool>(0b01100101 & (1 << 5)); // 1
+  cout << "\nSet bit 4: 0b10100101 | (1 << 3) = "; print_bits(0b10100101 | (1 << 3));
+  cout << "Clear bit position 6: 0b10100101 & ~(1 << 5) = "; print_bits(0b10100101 & ~(1 << 5));
+  cout << "Clear the middle 4 bits: 0b10100101 (target) & ~0b00111100 (mask) = "; print_bits(0b10100101 & ~0b00111100);
+
+  return 0;
 }
