26486f496129741e1d0513eb9b335e7490dd1c4f
Mark Frymire <mark.frymire@icloud.com>
2026-08-22 18:28:09 -0400

    Update l-value and r-value demo

--- modules/02-pointers-and-memory/lvalue-rvalue/lvalue-rvalue.cpp
+++ modules/02-pointers-and-memory/lvalue-rvalue/lvalue-rvalue.cpp
@@ -1,6 +1,9 @@
-
 // Exercises to help understand lvalues versus rvalues.
 // See here: https://www.youtube.com/watch?v=fbYknr-HPYE
+
+// ReSharper disable CppParameterMayBeConst
+// ReSharper disable CppParameterMayBeConstPtrOrRef
+// ReSharper disable CppPassValueParameterByConstReference
 
 #include <iostream>
 using std::cout;
@@ -9,77 +12,81 @@ using std::endl;
 #include <string>
 using std::string;
 
-// Here, x can be an lvalue or rvalue.
-void SetValuePassByValue(int x) { cout << x << endl; }
 
-// Here, x can only be an lvalue.
-void SetValuePassByReference(int& x) { cout << x << endl; }
+namespace {
 
-// Here, x can be an lvalue or rvalue.
-void SetValuePassByConstReference(const int& x) { cout << x << endl; }
+  // Here, x can be an lvalue or rvalue.
+  void set_value_by_value(int x) { cout << x << endl; }
 
-// Returning a reference to an int makes this function return an lvalue.
-int& GetValue() {
-  static int x = 10;
-  return x;
-}
+  // Here, x can only be an lvalue.
+  void set_value_by_reference(int& x) { cout << x << endl; }
 
-// Repeat with strings.
-void PrintNamePassByValue(string name) { cout << "either = " << name << endl; }
-void PrintNamePassByReference(string& name) { cout << "lvalue = " << name << endl;  }
-void PrintNamePassByConstReference(const string& name) { cout << "either = " <<name << endl; }
+  // Here, x can be an lvalue or rvalue.
+  void set_value_by_const_reference(const int& x) { cout << x << endl; }
 
-// To require a rvalue reference, use &&. Facilitates optimizations.
-void PrintNamePassByRValueReference(const string&& name) { cout << "rvalue = " << name << endl; }
+  // Returning a reference to an int makes this function return an lvalue.
+  int& get_value() {
+    static int x = 10;
+    return x;
+  }
+
+  // Repeat with strings.
+  void print_name_by_value(string name) { cout << "either = " << name << endl; }
+  void print_name_by_reference(string& name) { cout << "lvalue = " << name << endl; }
+  void print_name_by_const_reference(const string& name) { cout << "either = " << name << endl; }
+
+  // To require a rvalue reference, use &&. Facilitates optimizations.
+  void print_name_by_r_value(const string&& name) { cout << "rvalue = " << name << endl; }
+
+} // namespace
+
 
 int main() {
 
   int i = 10;
   //int& j = 10; // compile error, can't have an lvalue reference of an rvalue
-  const int& j = 10; // okay to have a *const* lvalue reference of an rvalue
+  [[maybe_unused]] const int& j = 10; // okay to have a *const* lvalue reference of an rvalue
 
   cout << "Pass by value...\n";
-  SetValuePassByValue(i); // x is an lvalue;
-  SetValuePassByValue(20); // x is an rvalue;
+  set_value_by_value(i); // x is an lvalue
+  set_value_by_value(20); // x is an rvalue
 
   cout << "\nPass by reference...\n";
-  SetValuePassByReference(i); // x is an lvalue;
-  //SetValuePassByReference(20); // compile error, can't pass a reference to an rvalue
-  
-  cout << "\nPass by const reference...\n";
-  SetValuePassByConstReference(i);
-  SetValuePassByConstReference(20);
+  set_value_by_reference(i); // x is an lvalue
+  //set_value_by_reference(20); // compile error, can't pass a reference to an rvalue
 
+  cout << "\nPass by const reference...\n";
+  set_value_by_const_reference(i);
+  set_value_by_const_reference(20);
 
   // Repeat with strings.
 
-  string first_name = "Mark";
-  string last_name = "Frymire";
+  const string first_name = "Mark";
+  const string last_name = "Frymire";
   string full_name = first_name + last_name;
 
   cout << "Pass by value...\n";
-  PrintNamePassByValue(full_name); // passing lvalue
-  PrintNamePassByValue(first_name + last_name); // passing rvalue
+  print_name_by_value(full_name); // passing lvalue
+  print_name_by_value(first_name + last_name); // passing rvalue
 
   cout << "\nPass by reference...\n";
-  PrintNamePassByReference(full_name); // passing lvalue
-  //PrintNamePassByReference(first_name + last_name); // compile error, can't pass rvalue by reference
-  
-  cout << "\nPass by const reference...\n";
-  PrintNamePassByConstReference(full_name); // okay to pass rvalue by const reference
-  PrintNamePassByConstReference(first_name + last_name); // okay to pass rvalue by const reference
-  
-  cout << "\nPass by rvalue reference...\n";
-  //PrintNamePassByRValueReference(full_name); // compiler error, must pass rvalue reference
-  PrintNamePassByRValueReference(first_name + last_name); // okay, passing rvalue reference
+  print_name_by_reference(full_name); // passing lvalue
+  //print_name_by_reference(first_name + last_name); // compile error, can't pass rvalue by reference
 
+  cout << "\nPass by const reference...\n";
+  print_name_by_const_reference(full_name); // okay to pass rvalue by const reference
+  print_name_by_const_reference(first_name + last_name); // okay to pass rvalue by const reference
+
+  cout << "\nPass by rvalue reference...\n";
+  // print_name_by_r_value(full_name); // compile error, must pass rvalue reference
+  print_name_by_r_value(first_name + last_name); // okay, passing rvalue reference
 
   // Now do tricks with function returns...
 
   cout << "\nReturn by rvalue...\n";
-  cout << GetValue() << endl;
+  cout << get_value() << endl;
 
   cout << "\nReturn by lvalue...\n";
-  GetValue() = 5;
-  cout << GetValue() << endl << endl;
+  get_value() = 5;  // the method returns the reference to the internal static which we then set
+  cout << get_value() << endl << endl;
 }
