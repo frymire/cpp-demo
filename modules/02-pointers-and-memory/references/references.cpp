e1c2a8ee72207184104fcd852cf3a60e00a8fd54
Mark Frymire <mark.frymire@icloud.com>
2026-08-22 16:17:25 -0400

    Refactor references demo

--- modules/02-pointers-and-memory/references/references.cpp
+++ modules/02-pointers-and-memory/references/references.cpp
@@ -3,16 +3,18 @@
 using std::cout;
 using std::endl;
 
-// This is the long way of passing data by reference to a function.
-void ElaborateIncrement(int* p_value) { (*p_value)++; }
+namespace {
 
-// Here's a cleaner way to do the same thing.
-void Increment(int& value) { value++;  }
+  // This is the long way of passing data by reference to a function.
+  void increment_elaborately(int* p_value) { (*p_value)++; }
+
+  // Here's a cleaner way to do the same thing.
+  void increment(int& value) { value++;  }
+}
 
 int main() {
 
   int a = 5;
-  int b = 8;
   cout << a << endl;
 
   // Create an alias to the variable a.
@@ -20,9 +22,9 @@ int main() {
   ref = 2;
   cout << a << endl;
 
-  ElaborateIncrement(&a);
+  increment_elaborately(&a);
   cout << a << endl;
 
-  Increment(a);
+  increment(a);
   cout << a << endl;
 }
