add729ae9bab56d79ff83c692efa636dc4c9b9fc
Mark Frymire <mark.frymire@icloud.com>
2026-08-22 13:47:59 -0400

    Refactor unions demo

--- modules/01-basics/unions/unions.cpp
+++ modules/01-basics/unions/unions.cpp
@@ -6,29 +6,29 @@
 using std::cout;
 using std::endl;
 
-struct Vector2 { 
+struct Vector2D {
   float x, y; 
-  void Print() { cout << x << " " << y << endl; }
+  void print() { cout << x << " " << y << endl; }
 };
 
 // Can treat the memory underlying a union as being of any declared type.
-struct Vector4 {
+struct Vector4D {
   union {
     struct { float x, y, z, w; };
-    struct { Vector2 a, b; };
+    struct { Vector2D a, b; };
   };
 };
 
 int main() {
 
   // Instantiate a Vector4, but access it as a pair of Vector2s.
-  Vector4 v4 = {1.0f, 2.0f, 3.0f, 4.0f};
-  v4.a.Print();
-  v4.b.Print();
+  Vector4D v4 = {1.0f, 2.0f, 3.0f, 4.0f};
+  v4.a.print();
+  v4.b.print();
   cout << endl;
 
   // Update a value while treating it as a Vector4.
   v4.z = 500.0f;
-  v4.a.Print();
-  v4.b.Print();
+  v4.a.print();
+  v4.b.print();
 }
