cc817b1f5d701012400f0860f0038e2279db1523
Mark Frymire <mark.frymire@icloud.com>
2026-08-22 17:11:30 -0400

    Refactor cyclical weak pointers demo

--- modules/02-pointers-and-memory/weak-pointers-cyclical/weak_pointers_cyclical.cpp
+++ modules/02-pointers-and-memory/weak-pointers-cyclical/weak_pointers_cyclical.cpp
@@ -1,52 +1,60 @@
 // https://www.youtube.com/watch?v=M0GLQEfplxs (12:45)
 
 #include <print>
+using std::print;
+
 #include <memory>
+using std::shared_ptr;
+using std::make_shared;
+using std::weak_ptr;
 
-struct B;
+namespace {
 
-struct A {
-  std::shared_ptr<B> p_B;
-  A() { std::print("A created.\n");}
-  ~A() { std::print("A destroyed.\n");}
-};
+  struct B;  // forward declaration so B can appear in the definition of A
 
-struct B {
-  std::shared_ptr<A> p_A;
-  B() { std::print("A created.\n");}
-  ~B() { std::print("A destroyed.\n");}
-};
+  struct A {
+    shared_ptr<B> p_B;
+    A() { print("A created.\n");}
+    ~A() { print("A destroyed.\n");}
+  };
 
+  struct B {
+    shared_ptr<A> p_A;
+    B() { print("B created.\n");}
+    ~B() { print("B destroyed.\n");}
+  };
 
-struct B2;
+  struct B2;
 
-struct A2 {
-  std::shared_ptr<B2> p_B;
-  A2() { std::print("A created.\n");}
-  ~A2() { std::print("A destroyed.\n");}
-};
+  struct A2 {
+    shared_ptr<B2> p_B;
+    A2() { print("A created.\n");}
+    ~A2() { print("A destroyed.\n");}
+  };
 
-struct B2 {
-  std::weak_ptr<A2> p_A;
-  B2() { std::print("A created.\n");}
-  ~B2() { std::print("A destroyed.\n");}
-};
+  struct B2 {
+    weak_ptr<A2> p_A;
+    B2() { print("B created.\n");}
+    ~B2() { print("B destroyed.\n");}
+  };
+
+} // namespace
 
 int main() {
 
   // At the end of this scope, neither object gets destroyed, due to the cyclical reference A <-> B
   {
-    std::shared_ptr<A> a = std::make_shared<A>();
-    std::shared_ptr<B> b = std::make_shared<B>();
+    const shared_ptr<A> a = make_shared<A>();
+    const shared_ptr<B> b = make_shared<B>();
     a->p_B = b;
     b->p_A = a;
   }
 
   // But here, it works, because the weak pointer in B2 doesn't prevent the A2 instance from dying.
-  std::print("\n");
+  print("\n");
   {
-    std::shared_ptr<A2> a = std::make_shared<A2>();
-    std::shared_ptr<B2> b = std::make_shared<B2>();
+    const shared_ptr<A2> a = make_shared<A2>();
+    const shared_ptr<B2> b = make_shared<B2>();
     a->p_B = b;
     b->p_A = a;
   }
