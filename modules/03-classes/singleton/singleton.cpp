49f068c198f1d92869d84480a5aae809866877d6
Mark Frymire <mark.frymire@icloud.com>
2026-08-24 14:09:52 -0400

    Update the singleton demo

--- modules/03-classes/singleton/singleton.cpp
+++ modules/03-classes/singleton/singleton.cpp
@@ -3,47 +3,52 @@
 using std::cout;
 using std::endl;
 
-class Singleton {
+namespace {
 
-private:
+  class Singleton {
 
-  // Prevent client code from instantiating this class by making the constructor private.
-  Singleton() {}
+  private:
 
-  // Just to prove we can do it, declare a mutable private member variable. Without 
-  // this, we could have just used a static function.
-  char* m_message = (char*) "Hi, from the singleton.";
+    // Prevent client code from instantiating this class by making the constructor private.
+    Singleton() {}
 
-  // Provide the meat behind the public interface.
-  void PrintMessageImpl() { cout << m_message << endl; }
+    // Just to prove we can do it, declare a mutable private member variable (m_message itself is still mutable).
+    // Without this, we could have just used a static function.
+    const char* m_message = "Hi, from the singleton.";
 
-public:
+    // Provide the meat behind the public interface.
+    void print_impl() const { cout << m_message << endl; }
 
-  // Provide access a singleton instance that is instantiated as a static variable within Get().
-  static Singleton& Get() { 
-    static Singleton s_instance;
-    return s_instance; 
-  }
+  public:
 
-  // Delete the copy constructor so you can't create a second instance like this...
-  //   Singleton another_singleton = Singleton::Get();
-  Singleton(const Singleton&) = delete; // C++11
+    // Provide access a singleton instance that is instantiated as a static variable within Get().
+    static Singleton& get() {
+      static Singleton s_instance;
+      return s_instance;
+    }
 
-  // Delete the assignment operator.
-  Singleton& operator=(const Singleton&) = delete;
+    // Delete the copy constructor so you can't create a second instance as shown below. This also suppresses the
+    // compiler's implicit move constructor, so you don't need to delete that explicitly.
+    //   Singleton another_singleton = Singleton::Get();
+    Singleton(const Singleton&) = delete; // C++11
 
-  // Since calling Singleton::Get() is a bit annoying, you can wrap up the ::Get()
-  // into public methods that are paired with private implementation methods.
-  static void PrintMessage() { return Get().PrintMessageImpl(); }
-};
+    // Delete the assignment operator. This also suppresses the compiler's implicit move assignment operator.
+    Singleton& operator=(const Singleton&) = delete;
+
+    // Since calling Singleton::get() is a bit annoying, you can wrap up the ::get()
+    // into public methods that are paired with private implementation methods.
+    static void print() { return get().print_impl(); }
+  };
+
+} // namespace
 
 
 int main() {
 
   // Call methods directly on the singleton like this.
-  Singleton::PrintMessage();
+  Singleton::print();
 
   // Or, make a variable to reference the singleton and treat it like any other instance.
-  Singleton& s = Singleton::Get();
-  s.PrintMessage();
+  Singleton& s = Singleton::get();
+  s.print();
 }
