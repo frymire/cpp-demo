90076c9b8f138a7679ef0824b7616c8cd3497ba6
Mark Frymire <mark.frymire@icloud.com>
2026-08-24 15:52:58 -0400

    Refactor static demo

--- modules/03-classes/static/external_variables.cpp
+++ modules/03-classes/static/external_variables.cpp
@@ -3,20 +3,21 @@
 #include "header_with_static.h"
 
 // These would be linker errors, due to the duplicate declarations in static.cpp.
-//int global = 5; 
-//void DuplicateFunction() {}
+// int global = 5;
+// void duplicate_function() {}
 
 // This was already declared in static.cpp, but not initialized. Because it 
 // was declared in main with "extern" there is no linker error.
+// ReSharper disable once CppUseInternalLinkage
 int externally_linked = 5;
 
 // The declaration of this variable in main can't find this, because
 // static effectively makes it "private" to this file. Linker error.
-//static int externally_linked_to_static = 10;
+static int externally_linked_to_static = 10;
 
 // Redefining these here is okay, despite being declared in static.cpp.
 // Declaring these as static means that the linker doesn't look outside of 
 // this file. This is beneficial for limiting the scope of variables and 
 // functions that would otherwise be global.
 static int s_my_static_int = 10;
-static void StaticFunction() {}
+static void static_function() {}
--- modules/03-classes/static/static.cpp
+++ modules/03-classes/static/static.cpp
@@ -11,16 +11,28 @@ using std::endl;
 // This line is okay, even with the same line in external_variables.cpp.
 #include "header_with_static.h"
 
+// ReSharper disable once CppUseInternalLinkage
 int global = 10;
-void DuplicateFunction() {}
 
+// ReSharper disable once CppUseInternalLinkage
+void duplicate_function() {}
+
+// Even though this wasn't initialized here, we can use it in main below, because we initialize it in
+// external_variables.cpp. Without extern, you get a multiple definitions linker error. With static, it compiles and
+// runs, but the value is 0 rather than 5, since it is default initialized here locally.
 extern int externally_linked;
 
-//extern int externally_linked_to_static;
+// You will get a linker error if you attempt to use this (see in main() below). It's defined as static in
+// external_variables.cpp, and therefore not visible from here.
+extern int externally_linked_to_static;
 
 static int s_my_static;
-void StaticFunction() {}
 
+// ReSharper disable once CppUseInternalLinkage
+void static_function() {}
+
+
+// ReSharper disable once CppUseInternalLinkage
 class Entity {
 
 public:
@@ -28,15 +40,16 @@ public:
   static int m_static;
   int m_non_static;
 
+  // ReSharper disable once CppPossiblyUninitializedMember
   Entity() { m_static++; }
 
-  static void PrintStatic() { cout << "m_static = " << m_static << endl; }
-  void PrintNonStatic() { cout << m_non_static << " " << m_static << endl; }
+  static void print_static() { cout << "m_static = " << m_static << endl; }
+  [[maybe_unused]] void print_non_static() const { cout << m_non_static << " " << m_static << endl; }
 
   // Static methods can't access non-static variables. Those variables are tied to 
   // a specific instance, but static methods aren't parameterized by instances. 
   // Declaring a method as static is almost equivalent to writing it in global scope.
-  //static void BadPrintStatic() { cout << m_non_static << endl; } // compile error
+  //static void bad_print_static() { cout << m_non_static << endl; } // compile error
 };
 
 // Have to explicitly declare static variables within classes/structs. Otherwise,
@@ -44,9 +57,10 @@ public:
 int Entity::m_static = 0;
 
 // Show what happens with static in a local scope.
-void FunctionWithLocalStatic() {
+// ReSharper disable once CppUseInternalLinkage
+void function_with_local_static() {
   static int n_calls = 0; // only initialized the first time
-  cout << "Number of calls to FunctionWithLocalStatic() = " << ++n_calls << endl;
+  cout << "Number of calls to function_with_local_static() = " << ++n_calls << endl;
 }
 
 int main() {
@@ -55,26 +69,26 @@ int main() {
   cout << "s_variable_in_header = " << s_variable_in_header << endl;
   cout << "global = " << global << endl; 
   cout << "externally_linked = " << externally_linked << endl;
-  //cout << externally_linked_to_static << endl;
+  // cout << externally_linked_to_static << endl;  // uncommenting this leads to a undefined reference linker error
   cout << "s_my_static = " << s_my_static << endl << endl; // prints 0
 
   // Show what happens with static withing of classes/structs.
-  Entity::PrintStatic(); // 0
+  Entity::print_static(); // 0
   Entity e1;
-  e1.PrintStatic(); // 1
+  e1.print_static(); // 1
   Entity e2;
-  e2.PrintStatic(); // 2
+  e2.print_static(); // 2
 
   // Careful! Here, it looks like you're assigning to an instance, but you are 
   // actually assigning to variables within the class/struct itself.
   e2.m_static = 5; // equivalent to Entity::m_static = 5;
-  e2.PrintStatic(); // 5
-  e1.PrintStatic(); // 5!
-  Entity::PrintStatic(); // 5
+  e2.print_static(); // 5
+  e1.print_static(); // 5!
+  Entity::print_static(); // 5
 
   // Show what happens with static in a local scope.
   cout << endl;
-  FunctionWithLocalStatic();
-  FunctionWithLocalStatic();
-  FunctionWithLocalStatic();
+  function_with_local_static();
+  function_with_local_static();
+  function_with_local_static();
 }
