0529ca8ce0c8cbb2844fda815116336b85cedfd6
Mark Frymire <mark.frymire@icloud.com>
2026-08-24 17:16:26 -0400

    Update templates demo

--- modules/04-functions-and-generics/templates/templates.cpp
+++ modules/04-functions-and-generics/templates/templates.cpp
@@ -6,32 +6,35 @@
 using std::cout;
 using std::endl;
 
-template<typename T> void Print(T value) { cout << value << endl; }
+namespace {
+  template<typename T> void print(T value) { cout << value << endl; }
 
-// Unlike Java generics, which depend on the type system, C++ templates let you pass 
-// other categories of information. Here, use it to create whole class types on the fly.
-// You're effectively programming what the compiler will do during compile-time.
-template<typename T, int N> 
-class Array {
-private:
-  T m_array[N];
-public:
-  T GetSize() const { return N; }
-};
+  // Unlike Java generics, which depend on the type system, C++ templates let you pass
+  // other categories of information. Here, use it to create whole class types on the fly.
+  // You're effectively programming what the compiler will do during compile-time.
+  template<typename T, int N>
+  class Array {
+  private:
+    T m_array[N];
+  public:
+    // ReSharper disable once CppMemberFunctionMayBeStatic
+    [[nodiscard]] T get_size() const { return N; }
+  };
+}
 
 int main() {
 
-  // Implicit calls.
-  Print(5);
-  Print("Hello");
-  Print(5.5f);
+  // Implicit calls
+  print(5);
+  print("Hello");
+  print(5.5f);
 
-  // Explicit calls.
-  Print<int>(5);
-  Print<const char*>("Hello");
-  Print<float>(5.5f);
+  // Explicit calls
+  print<int>(5);
+  print<const char*>("Hello");
+  print<float>(5.5f);
 
   // Pass an array type and size via the template argument.
-  Array<int, 10> my_array;
-  cout << endl << my_array.GetSize() << endl;
+  constexpr Array<int, 10> my_array{};
+  cout << endl << my_array.get_size() << endl;
 }
