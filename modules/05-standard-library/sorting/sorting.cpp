1d033b44a101adac53b43140cd3d5a54c7533578
Mark Frymire <mark.frymire@icloud.com>
2026-08-25 10:13:59 -0400

    Update sorting demo

--- modules/05-standard-library/sorting/sorting.cpp
+++ modules/05-standard-library/sorting/sorting.cpp
@@ -3,57 +3,86 @@
 // See here: https://www.youtube.com/watch?v=x0uUKWJzSO4&list=WL&index=26
 
 // ReSharper disable CppUseRangeAlgorithm
-// ReSharper disable CppCStyleCast
 
 #include <stdio.h>  // printf
 
 // ReSharper disable once CppUnusedIncludeDirective
-#include <stdlib.h> // qsort
+#include <stdlib.h>  // qsort
 
 #include <iostream>
 using std::cout;
 using std::endl;
 
-#include <vector>
-using std::vector;
+#include <array>
+using std::array;
 
 #include <algorithm>
-using std::sort;
+using std::ranges::sort;
 
-namespace
-{
+#include <functional>
+using std::function;
+
+namespace {
+
   int compare_ints(const void* a, const void* b) {
-    return (*(int*) a - *(int*) b);
+    const int x = *static_cast<const int*>(a);
+    const int y = *static_cast<const int*>(b);
+    return (x - y);
   }
-}
+
+  void print_values(const array<int, 5>& values) { for (const int v : values) { cout << v << " "; } }
+
+} // namespace
 
 int main() {
 
-  // Quick sort
-  int values[] = {40, 10, 100, 90, 20, 25};
-  qsort(values, 6, sizeof(int), compare_ints);
-  for (int n = 0; n < 6; n++) printf("%d\n", values[n]);
-  printf("\n");
+  cout << "\nSort in ascending order by default...\n";
+  {
+    array values = {3, 5, 1, 4, 2};
+    sort(values /*, std::less<int>() */);
+    print_values(values);
+  }
 
-  // Use std::sort to sort in ascending order by default.
-  vector<int> values2 = {3, 5, 1, 4, 2};
-  sort(values2.begin(), values2.end());
-  for (int v : values2) { cout << v << endl; }
+  cout << "\n\nOnly sort the first 3 elements...\n";
+  {
+    array values = {3, 5, 1, 4, 2};
+    std::sort(values.begin(), values.begin() + 3);
+    print_values(values);
+  }
+
+  cout << "\n\nSort in descending order...\n";
+  {
+    array values = {3, 5, 1, 4, 2};
+    sort(values, std::greater());
+    print_values(values);
+  }
+
+  cout << "\n\nUse a lambda to sort in descending order...\n";
+  {
+    array values = {3, 5, 1, 4, 2};
+    sort(values, [](const int a, const int b) { return a > b; });
+    print_values(values);
+  }
+
+  cout << "\n\nUse a lambda to provide an ascending sort, except that 1 goes to the end...\n";
+  {
+    array values = {3, 5, 1, 4, 2};
+    const function<bool(int, int)> /*auto*/ special = [](const int a, const int b) {
+      if (a == 1) return false;
+      if (b == 1) return true;
+      return a < b;
+    };
+    sort(values, special);
+    print_values(values);
+  }
+
+  cout << "\n\nSort ascending with qsort...\n";
+  {
+    int values[] = {40, 10, 100, 90, 20, 25};
+    std::qsort(values, 6, sizeof(int), compare_ints);
+    for (int n = 0; n < 6; n++) cout << values[n] << " ";
+  }
+
   cout << endl;
-
-  // Use a lambda to sort in descending order.
-  vector<int> values3 = {3, 5, 1, 4, 2};
-  sort(values3.begin(), values3.end(), [](int a, int b) { return a > b; });
-  for (int v : values3) { cout << v << endl; }
-  cout << endl;
-
-  // Use a lambda to provide an ascending sort, except that 1 goes to the end.
-  vector<int> values4 = {3, 5, 1, 4, 2};
-  auto special = [](int a, int b) { 
-    if (a == 1) return false;
-    if (b == 1) return true;
-    return a < b; 
-  };
-  sort(values4.begin(), values4.end(), special);
-  for (int v : values4) { cout << v << endl; }
+  return 0;
 }
