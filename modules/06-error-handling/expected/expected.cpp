b9c10c96cf6b5b4afc3761a8fd9e3517c1d564b0
Mark Frymire <mark.frymire@icloud.com>
2026-08-25 22:48:14 -0400

    Fix expected demo

--- modules/06-error-handling/expected/expected.cpp
+++ modules/06-error-handling/expected/expected.cpp
@@ -1,28 +1,58 @@
 // https://www.youtube.com/watch?v=Vz40rDiWnN8&list=WL&index=4
 
-#include <expected>
 #include <iostream>
+using std::cout;
 
-std::expected<int, std::string> divide(int a, int b) {
-    if (b == 0) { return std::unexpected("Division by zero."); }
-    return a / b;
-}
+#include <expected>
+using std::expected;
+using std::unexpected;
 
-std::expected<int, std::string> handle_error(const std::string& error) {
-    std::cout << "error: " << error << "\n";
-    return std::expected<int, std::string>({0});
-}
+#include <string>
+using std::string;
+
+namespace {
+
+    expected<int, string> divide(const int a, const int b) {
+        if (b == 0) { return unexpected("Division by zero."); }
+        return a / b;
+    }
+
+    expected<int, string> handle_error(const string& error) {
+        cout << "error: " << error << "\n";
+        return expected<int, string>({0});  // return default value
+    }
+
+}  // namespace
 
 int main() {
 
-    auto result1 = divide(10, 2);
-    if (!result1) { std::expected<int, std::string> default_value = handle_error(result1.error()); }
-    std::cout << "result: " << *result1 << "\n";
+    {
+        cout << "\nExpected result...\n";
+        expected<int, string> /*auto*/ result = divide(10, 3);
+        if (!result) { result = handle_error(result.error()); }
+        cout << "result: " << *result << "\n";
+    }
 
-    auto result2 = divide(24, 3)
-      .and_then([](int result) { return divide(result, 4); })  // chain multiple operations
-      .or_else(handle_error);
-    std::cout << "result: " << *result2 << "\n";
+    {
+        cout << "\nUnexpected result...\n";
+        auto result = divide(10, 0);
+        if (!result) { result = handle_error(result.error()); }
+        cout << "result: " << *result << "\n";
+    }
+
+    {
+        cout << "\nMore succinctly...\n";
+        cout << "result: " << *divide(10, 3).or_else(handle_error) << "\n";
+        cout << "result: " << *divide(10, 0).or_else(handle_error) << "\n";
+    }
+
+    {
+        cout << "\nChain multiple operations: 24 / 3 / 4...\n";
+        const auto result = divide(24, 3)
+         .and_then([](const int x) { return divide(x, 4); })
+         .or_else(handle_error);
+        cout << "result: " << *result << "\n";
+    }
 
     return 0;
 }
