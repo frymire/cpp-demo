6de104e1afb2c4c0ae67cb133fb6b0d21a452aad
Mark Frymire <mark.frymire@icloud.com>
2026-08-25 22:30:14 -0400

    Update exceptions demo

--- modules/06-error-handling/exceptions/exceptions.cpp
+++ modules/06-error-handling/exceptions/exceptions.cpp
@@ -15,22 +15,26 @@ using std::invalid_argument;
 
 #include<assert.h>
 
-void MyFunction(int value) {
+namespace {
 
-  // Use assert to check errors for which you will not provide error handling.
-  //assert(value < numeric_limits<char>::max()); // runtime assertion failure
+  void my_function(const int value) {
 
-  // Use exceptions if the caller might provide error handling.
-  if (value > numeric_limits<char>::max()) {
-    throw invalid_argument("MyFunction argument too large."); // throw exceptions by value
-  }    
-}
+    // Use assert to check errors for which you will not provide error handling.
+    // assert(value < numeric_limits<char>::max()); // runtime assertion failure, only in debug mode
+
+    // Use exceptions if the caller might provide error handling.
+    if (value > numeric_limits<char>::max()) {
+      throw invalid_argument("MyFunction argument too large."); // throw exceptions by value
+    }
+  }
+
+} // namespace
 
 int main() {
 
   try {
-    MyFunction(256); // cause an exception
-  } 
+    my_function(256); // cause an exception
+  }
   catch (invalid_argument& e) { // catch exceptions by reference
     cerr << e.what() << endl;
     return -1;
