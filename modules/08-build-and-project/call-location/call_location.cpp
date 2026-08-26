2ca45468ee1683d1cbfbfe089cfa286b5df8020f
Mark Frymire <mark.frymire@icloud.com>
2026-08-26 00:10:39 -0400

    Update call locations demo

--- modules/08-build-and-project/call-location/call_location.cpp
+++ modules/08-build-and-project/call-location/call_location.cpp
@@ -4,20 +4,23 @@
 #include <source_location>
 
 #if defined(__clang__) || defined(__GNUC__)
-  #define FUNCTION_SIGNATURE __PRETTY_FUNCTION__
+#define FUNCTION_SIGNATURE __PRETTY_FUNCTION__
 #elif defined(_MSC_VER)
-  #define FUNCTION_SIGNATURE __FUNCSIG__
+#define FUNCTION_SIGNATURE __FUNCSIG__
 #else
-  #define FUNCTION_SIGNATURE __func__
+#define FUNCTION_SIGNATURE __func__
 #endif
 
-template <typename T>
-void print_call_location(
-  [[maybe_unused]] const T& value,
-  std::source_location where = std::source_location::current()
-) {
-  std::println("[{}:{}] {}", where.file_name(), where.line(), FUNCTION_SIGNATURE);
-}
+namespace {
+
+  template <typename T>
+  void print_call_location(
+    [[maybe_unused]] const T& value,
+    const std::source_location where = std::source_location::current()) {
+    std::println("[{}:{}] {}", where.file_name(), where.line(), FUNCTION_SIGNATURE);
+  }
+
+}  // namespace
 
 int main() {
   print_call_location(42);
