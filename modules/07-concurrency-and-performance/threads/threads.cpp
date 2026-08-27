6b67063d39d6cb1652961ec07c68e957190a2831
Mark Frymire <mark.frymire@icloud.com>
2026-08-26 20:20:13 -0400

    Update threads demo

--- modules/07-concurrency-and-performance/threads/threads.cpp
+++ modules/07-concurrency-and-performance/threads/threads.cpp
@@ -1,4 +1,3 @@
-
 // Demo threads.
 // See here: https://www.youtube.com/watch?v=wXBcwHwIt_I&list=WL&index=71
 
@@ -17,14 +16,15 @@ using std::vector;
 using std::thread;
 using std::this_thread::sleep_for;
 
+#include <atomic>
+using std::atomic;
+
 #include <chrono>
 using namespace std::chrono_literals; // define time, for instance, as "1s"
 
-
-
-static std::atomic s_enter_was_pressed = false;
-
 namespace {
+
+  atomic s_enter_was_pressed = false;
 
   void do_work(const int id) {
     osyncstream(cout) << "Started local thread ID = " << id << endl;
@@ -49,11 +49,15 @@ int main() {
   vector<thread> threads;
   for (int id = 0; id < kNumThreads; ++id) { threads.emplace_back(do_work, id); }
 
+  // Or, to start a single thread...
+  thread late_worker{do_work, kNumThreads};
+
   // Block on the main thread until the user presses enter.
   cin.get();
   s_enter_was_pressed = true;
 
   // Wait for the workers to complete their work and rejoin the main thread.
   for (thread& thread: threads) { thread.join(); }
+  late_worker.join();
   cout << "Finished main thread." << endl;
 }
